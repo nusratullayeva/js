53bae76e9089d2cfadd5bae3ddfc86cf76bda3b9
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-03-03 06:28:48 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function students(personstudents) {
+    return personstudents / 2500 * 100;
+}
+console.log(students(24));
+console.log(students(28));
+console.log(students(30));
+console.log(students(40));
